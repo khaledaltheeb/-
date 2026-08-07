98aadf31a7e1ce16c054aa148db05d6ef6971ff9
khaledaltheeb <63592172+khaledaltheeb@users.noreply.github.com>
2026-08-07 19:13:10 +0300

    Use Node runtime for Twitter image

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,6 +1,5 @@
 import OpenGraphImage, { alt, size, contentType } from './opengraph-image';
 
-export const runtime = 'edge';
 export { alt, size, contentType };
 
 export default function TwitterImage() {
