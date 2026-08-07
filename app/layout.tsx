ff3dc9e5124227c2f87babb21eee30b452189d89
khaledaltheeb <63592172+khaledaltheeb@users.noreply.github.com>
2026-08-07 19:11:51 +0300

    Load polished public profile theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ import './system-states.css';
 import './content-v3.css';
 import './structured-content.css';
 import './block-editor-v3.css';
+import './profile-v3.css';
 
 const arabicFont = Noto_Sans_Arabic({
   subsets: ['arabic'],
