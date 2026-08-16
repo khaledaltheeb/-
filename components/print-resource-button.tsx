b7fbf6891775c6c43e69eb517799e7b33a38db91
khaledaltheeb <63592172+khaledaltheeb@users.noreply.github.com>
2026-08-16 06:37:52 +0300

    Restore printable legacy practical resources

---
+++ components/print-resource-button.tsx
@@ -0,0 +1,2 @@
+'use client';
+export default function PrintResourceButton(){return <button type="button" className="resource-print-button" onClick={()=>window.print()}>طباعة / حفظ PDF من المتصفح</button>}
