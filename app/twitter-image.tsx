9224e695b0dc1a05be9328be37030979b3034cb5
khaledaltheeb <63592172+khaledaltheeb@users.noreply.github.com>
2026-08-24 00:56:54 +0300

    seo: remove root Twitter image override so page metadata can win

--- app/twitter-image.tsx
+++
@@ -1,7 +0,0 @@
-import OpenGraphImage, { alt, size, contentType } from './opengraph-image';
-
-export { alt, size, contentType };
-
-export default function TwitterImage() {
-  return OpenGraphImage();
-}
