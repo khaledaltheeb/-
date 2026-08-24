685f70b96646853e96348c8e02497b8d908c794f
khaledaltheeb <63592172+khaledaltheeb@users.noreply.github.com>
2026-08-24 08:33:15 +0300

    Load GTM early in production root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,7 +121,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body id="top">
         {analyticsEnabled && gtmId ? (
           <>
-            <Script id="rawafid-gtm" strategy="afterInteractive">
+            <Script id="rawafid-gtm" strategy="beforeInteractive">
               {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');`}
             </Script>
             <noscript>
