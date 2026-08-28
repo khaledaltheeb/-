51e595caff7145839b330b938b9606ee857b7c90
khaledaltheeb <63592172+khaledaltheeb@users.noreply.github.com>
2026-08-28 20:46:14 +0300

    fix(android): enforce Java 17 compilation

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -39,6 +39,10 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     buildFeatures { buildConfig = true }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
 }
