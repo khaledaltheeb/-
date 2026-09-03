2a996f66c0a8b6fb507398ddd9b2b073ddd46f6c
khaledaltheeb <63592172+khaledaltheeb@users.noreply.github.com>
2026-09-03 08:33:18 +0300

    perf: defer GTM until after hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -126,7 +126,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body id="top">
         {analyticsEnabled && gtmId ? (
           <>
-            <Script id="rawafid-gtm" strategy="beforeInteractive">
+            <Script id="rawafid-gtm" strategy="afterInteractive">
               {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');`}
             </Script>
             <noscript>
